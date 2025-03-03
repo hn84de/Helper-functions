8a303fcbce04355f2c489df87b840eb29923646a
hn84de <118720399+hn84de@users.noreply.github.com>
2025-03-03 06:18:57 -0800

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,33 +1,33 @@
 function getCopy(objToCopy)
 {
+  if (objToCopy === null)
+  {
+    return objToCopy;
+  }
+  
   if (typeof objToCopy !== 'object')
   {
     return objToCopy;
   }
   
-  if (objToCopy === null)
-  {
-    return objToCopy;
-  }
-
   if (Array.isArray(objToCopy))
   {
     var retArr = [];
-    const arrSize = objToCopy;
+    const arrSize = objToCopy.length;
     for (var i = 0; i < arrSize; ++i)
-      {
-        retArr[i] = getCopy(objToCopy[i]);
-      }
+    {
+      retArr[i] = getCopy(objToCopy[i]);
+    }
     return retArr;
   }
 
   var retObj = {};
   for (const key in objToCopy)
+  {
+    if (objToCopy.hasOwnProperty(key))
     {
-      if (objToCopy.hasOwnProperty(key))
-      {
-        retObj[key] = getCopy(objToCopy[key]);
-      }
+      retObj[key] = getCopy(objToCopy[key]);
     }
+  }
   return retObj;
 }
